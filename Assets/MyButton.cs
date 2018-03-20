aecdb5bd0a30c5e80b3a325fd5e579811981ace9
Scott AIE <sloth.13@hotmail.com>
2018-03-20 11:52:04 +1030

    Inital broken push :)

---
+++ Assets/MyButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyButton :  Button {
+
+    public float m_timer = 0;
+
+    private bool m_isSelected = false;
+    public bool IsSelected
+    {
+        get
+        {
+            return m_isSelected;
+        }
+        set
+        {
+            m_isSelected = value;
+        }
+    }
+}
---
+++ Assets/MyButtonEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[RequireComponent(typeof(MyButton))]
+public class MyButtonEditor : UnityEditor.UI.ButtonEditor {
+    public override void OnInspectorGUI()
+    {
+        MyButton button = (MyButton)target;
+
+        base.OnInspectorGUI();
+
+        //button.m_timer = EditorGUI
+    }
+
+}
